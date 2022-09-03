f46aee33a9bc2662c9912c3ba12a0f8917446072
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2022-09-03 21:41:45 +0000

    holerite formatado

--- funcionario/holeriteGerarPdf.php
+++ funcionario/holeriteGerarPdf.php
@@ -1,122 +1,122 @@
 <?php
-require('../classes/vendor/setasign/fpdf/fpdf.php');
+require_once('../classes/vendor/tecnickcom/tcpdf/examples/tcpdf_include.php');
 
-class PDF extends FPDF
-{
-// Load data
-function LoadData($file)
-{
-    // Read file lines
-    $lines = file($file);
-    $data = array();
-    foreach($lines as $line)
-        $data[] = explode(';',trim($line));
-    return $data;
-}
+class MYPDF extends TCPDF {
 
-function LoadText($file)
-{
-    // Read file lines
-    // $lines = file($file);
-    $lines = $file;
-    $data = array();
-    foreach($lines as $line)
-        $data[] = explode(';',trim($line));
-    return $data;
-}
+    //Page header
+    public function Header() {
+        // Logo
+        $image_file = '../img/logoEntrada.svg';
+        $this->ImageSVG($image_file, 10, 0, 30, '', 'SVG', '', 'T', false, 300, '', false, false, 0, false, false, false);
+        // Set font
+        $this->SetFont('helvetica', 'B', 20);
+        // Title
+        $this->Cell(0, 15, 'Holerite do mês', 0, false, 'C', 0, '', 0, false, 'R', 'M');
+    }
 
-// Simple table
-function BasicTable($header, $data)
-{
-    // Header
-    foreach($header as $col)
-        $this->Cell(40,7,$col,1);
-    $this->Ln();
-    // Data
-    foreach($data as $row)
-    {
-        foreach($row as $col)
-            $this->Cell(40,6,$col,1);
+    // Load table data from file
+    public function LoadData($file) {
+        // Read file lines
+        $lines = file($file);
+        $data = array();
+        foreach($lines as $line) {
+            $data[] = explode(';', chop($line));
+        }
+        return $data;
+    }
+
+    // Colored table
+    public function ColoredTable($header,$data) {
+        // Colors, line width and bold font
+        $this->SetFillColor(53, 79, 82);
+        $this->SetTextColor(255);
+        $this->SetDrawColor(47, 62, 70);
+        $this->SetLineWidth(0.3);
+        $this->SetFont('', 'B');
+        // Header
+        $w = array(50, 50, 50);
+        $x = array(50,50);
+        $num_headers = count($header) -1;
+        for($i = 0; $i < $num_headers; ++$i) {
+            $this->Cell(50, 7, $header[$i], 1, 0, 'C', 1);
+        }
+        $this->Ln();
+        // Color and font restoration
+        $this->SetFillColor(202, 210, 197);
+        $this->SetTextColor(0);
+        $this->SetFont('');
+        // Data
+        $fill = 1;
+        foreach($data as $row) {
+            for($i = 0; $i < $num_headers; ++$i) {
+                $this->Cell($w[$i], 6, $row[$i], 'LR', 0, 'C', $fill);
+            }
+            
+            $this->Ln();
+            $fill=!$fill;
+        }
+        $this->Cell(50*$num_headers, 2, '', 'T');
         $this->Ln();
     }
 }
 
-// Better table
-function ImprovedTable($header, $data)
-{
-    // Column widths
-    $w = array(40, 35, 40, 45);
-    // Header
-    for($i=0;$i<count($header);$i++)
-        $this->Cell($w[$i],7,$header[$i],1,0,'C');
-    $this->Ln();
-    // Data
-    foreach($data as $row)
-    {
-        $this->Cell($w[0],6,$row[0],'LR');
-        $this->Cell($w[1],6,$row[1],'LR');
-        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-        $this->Ln();
-    }
-    // Closing line
-    $this->Cell(array_sum($w),0,'','T');
-}
+$pdf = new MYPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
-// Colored table
-function FancyTable($header, $data)
-{
-    // Colors, line width and bold font
-    $this->SetFillColor(255,0,0);
-    $this->SetTextColor(255);
-    $this->SetDrawColor(128,0,0);
-    $this->SetLineWidth(.3);
-    $this->SetFont('','B');
-    // Header
-    $w = array(40, 35, 40, 45);
-    for($i=0;$i<count($header);$i++)
-        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
-    $this->Ln();
-    // Color and font restoration
-    $this->SetFillColor(224,235,255);
-    $this->SetTextColor(0);
-    $this->SetFont('');
-    // Data
-    $fill = false;
-    foreach($data as $row)
-    {
-        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
-        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
-        $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
-        $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
-        $this->Ln();
-        $fill = !$fill;
-    }
-    // Closing line
-    $this->Cell(array_sum($w),0,'','T');
-}
-}
+$pdf->SetCreator(PDF_CREATOR);
+// $pdf->SetAuthor('Nicola Asuni');
+$pdf->SetTitle('holerite');
+// $pdf->SetSubject('TCPDF Tutorial');
+// $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 
-$pdf = new PDF();
-// Column headings
-$header[0] = array('Pagamentos', 'Descontos', 'Liquido');
 
-$header[1] = array('Sal. Base', 'Sal. Contrib. INSS', 'Base FGTS');
-$header[2] = array('FGTS do mês', 'Base Cálculo IRPF');
-// $header
-// Data loading
-$data[0] = $pdf->LoadData("dados1.txt");
+// set default header data
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_STRING);
+
+// set header and footer fonts
+$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+// set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+// set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+// set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+// set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+// set some language-dependent strings (optional)
+// if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
+//     require_once(dirname(__FILE__).'/lang/eng.php');
+//     $pdf->setLanguageArray($l);
+// }
+
+// ---------------------------------------------------------
+
+// set font
+$pdf->SetFont('helvetica', '', 12);
+
+// add a page
+$pdf->AddPage();
+
+$header[0] = array('Pagamentos', 'Descontos', 'Liquido','');
+$header[1] = array('Sal. Base', 'Sal. Contrib. INSS', 'Base FGTS','');
+$header[2] = array('FGTS do mês', 'Base Cálculo IRPF','');
+
+$data[0] = $pdf->LoadData("dados.txt");
 $data[1] = $pdf->LoadData("dados1.txt");
 $data[2] = $pdf->LoadData("dados2.txt");
 
-$pdf->SetFont('Arial','',14);
-$pdf->AddPage();
-$pdf->BasicTable($header[0],$data[0]);
-$pdf->BasicTable($header[1],$data[1]);
-$pdf->BasicTable($header[2],$data[2]);
-// $pdf->AddPage();
-// $pdf->ImprovedTable($header,$data);
-// $pdf->AddPage();
-// $pdf->FancyTable($header,$data);
-$pdf->Output();
+// // print colored table
+$pdf->ColoredTable($header[0], $data[0]);
+$pdf->ColoredTable($header[1], $data[1]);
+$pdf->ColoredTable($header[2], $data[2]);
+$pdf->Output('holerite.pdf', 'I');
+
 ?>
+
