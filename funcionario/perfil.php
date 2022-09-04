7ef7eb375855376334ed8e006de6834bab476f30
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-09-03 22:22:30 -0300

    Update perfil.php

--- funcionario/perfil.php
+++ funcionario/perfil.php
@@ -1,5 +1,9 @@
 <?php
     require_once '../configs/sessionAutentica.php';
+    if ($_SESSION['nome'] !== 'Administrador') {
+        header('Location: ../configs/sair.php');
+        die();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
