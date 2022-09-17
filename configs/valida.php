e63c82ceafe35e85b6a6546c98018a9eafbedeef
Welinton Martins <57647104+welinton-martins@users.noreply.github.com>
2022-09-17 01:46:02 +0000

    Ajuste Banco

--- configs/valida.php
+++ configs/valida.php
@@ -5,7 +5,7 @@
         session_start();
         $_SESSION['id'] = session_id();
         $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-        $_SESSION['erro'];
+        // $_SESSION['erro'];
     }
     $email = $_POST["email"];
     $senha = $_POST["senha"];
@@ -15,12 +15,12 @@
         $gestor = new PDO("mysql:host=".MYSQL_HOST.";"."dbname=".MYSQL_DATABASE.";charset=utf8",MYSQL_USER,MYSQL_PASS);
         $gestor->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $dado = $gestor->query("Select * FROM funcionario");
+        $dado = $gestor->query("SELECT * FROM funcionario");
         $valida = $dado->fetchAll(PDO::FETCH_ASSOC);
         if ($_SESSION['erro'] < 2 || $_SESSION['erro'] == null) { 
             foreach($valida as $val) {
-                if($email == $val['email'] && password_verify($senha, $val['senha']) == true && $val['funcionarioNome'] == 'Administrador'){
-                    $_SESSION['nome'] = $val['funcionarioNome'];
+                if($email == $val['email'] && password_verify($senha, $val['senha']) == true && $val['nome_funcionario'] == 'Administrador'){
+                    $_SESSION['nome'] = $val['nome_funcionario'];
                     $_SESSION['id_funcionario'] = $val['id_funcionario'];
                     $_SESSION['caminho'] = $val['caminho'];
                     if ($_SESSION['caminho'] == null){
@@ -32,8 +32,8 @@
                     header('Location: ../funcionario/painelGerente.php');
                     die();
                 }
-                if ($email == $val['email'] && password_verify($senha, $val['senha']) == true && $val['funcionarioNome'] != 'Administrador') {   
-                    $_SESSION['nome'] = $val['funcionarioNome'];
+                if ($email == $val['email'] && password_verify($senha, $val['senha']) == true && $val['nome_funcionario'] != 'Administrador') {   
+                    $_SESSION['nome'] = $val['nome_funcionario'];
                     $_SESSION['id_funcionario'] = $val['id_funcionario'];
                     $_SESSION['caminho'] = $val['caminho'];
                     if ($_SESSION['caminho'] == null){
@@ -47,17 +47,17 @@
                 }
             }
             $_SESSION['erro']++;
-            header('Location: ../errou.php');
+            // header('Location: ../errou.php');
             die();
         } else {
-            header('Location: ../redefine.php');
+            // header('Location: ../redefine.php');
             die();
         }
         
 
     } catch(PDOException $e) {    
         echo "Connection failed: " . $e->getMessage();
-        header('Location: sair.php');
+        // header('Location: sair.php');
         die();
     }
     
