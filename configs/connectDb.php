87e3b38ab127195abd19cdaf84d65fb816d41a11
Luiz Miguel <70536705+LuizMiguelSR@users.noreply.github.com>
2022-10-08 21:19:37 -0300

    Update connectDb.php

--- configs/connectDb.php
+++ configs/connectDb.php
@@ -1,8 +1,8 @@
 <?php
-    define('MYSQL_HOST','127.0.0.1');
-    define('MYSQL_DATABASE','fhr2');
-    define('MYSQL_USER','root');
-    define('MYSQL_PASS','');
+    define('MYSQL_HOST','31.170.163.156');
+    define('MYSQL_DATABASE','dgt_fhr');
+    define('MYSQL_USER','dgt_banco');
+    define('MYSQL_PASS','giLZLgN+$c-F');
 
     $conexao = new PDO("mysql:host=".MYSQL_HOST.";"."dbname=".MYSQL_DATABASE.";charset=utf8",MYSQL_USER,MYSQL_PASS);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
