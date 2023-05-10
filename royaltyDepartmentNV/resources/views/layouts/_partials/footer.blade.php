eba8beee1e74cbb426b6738fedf27487030e86ad
LuizMiguelSR <luizmiguel.srodrigues@gmail.com>
2023-05-10 17:58:44 -0300

    Update: Ajustes na media querie

--- royaltyDepartmentNV/resources/views/layouts/_partials/footer.blade.php
+++ royaltyDepartmentNV/resources/views/layouts/_partials/footer.blade.php
@@ -1,11 +1,9 @@
-<br><br><br><br><br>
-
 <style>
     .text-muted {
         color: #0b6567 !important;
     }
 </style>
 
-<footer class="mt-5">
+<footer class="mt-3">
     <p class="text-center text-muted">&copy; {{ date('Y') }} Royalty Department</p>
 </footer>
